eb7fb53b0aaab939b9c9dc3fe9921022ca807d7c
motazallala <www.hot122@gmail.com>
2023-08-09 05:10:31 +0300

    Add some component like search , product list and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  coloer = ['red', 'blue','green','yalow']
-  name= "motaz";
-  lastName = "allala";
-  ss='https://picsum.photos/seed/picsum/500/300';
-
-  myFun(){
-    console.log("this is test");
-    alert("this is test");
-  }
-  onChange(){
-    alert("test");
-  }
+  viewModel = "map";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { ContainerComponent } from './container/container.component';
+import { SearchComponent } from './container/search/search.component';
+import { ProductListComponent } from './container/product-list/product-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    HeaderComponent,
+    TopHeaderComponent,
+    TopMenuComponent,
+    MainMenuComponent,
+    ContainerComponent,
+    SearchComponent,
+    ProductListComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
